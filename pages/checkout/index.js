193ac3e52fecca68bcffea8068b09dc6acdf7d0e
st46593 <st46593@student.upce.cz>
2021-02-21 14:50:14 +0100

    form data-netlify

--- pages/checkout/index.js
+++ pages/checkout/index.js
@@ -364,7 +364,7 @@ class CheckoutPage extends Component {
                   </div>
                 </div>
                 {this.isNotCartEmpty(cart) && (
-                 <form name="order" netlify onSubmit={this.handleSubmit} onChange={this.handleChangeForm}>
+                 <form name="order" onSubmit={this.handleSubmit} onChange={this.handleChangeForm} data-netlify="true">
                     <input type="hidden" name="form-name" value="order" />
                     {/* ShippingDetails */}
                     <p className="font-size-subheader font-weight-semibold mb-4">
