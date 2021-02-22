609e8e77eea38849eb803905c52d6f99a73fe21a
st46593 <st46593@student.upce.cz>
2021-02-22 19:33:38 +0100

    footer back

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -2,6 +2,20 @@ import React from "react";
 
 const Footer = () => (
   <footer className="pt-5">
+    <form name="order" data-netlify="true" hidden>
+      <input type="hidden" name="form-name" value="order" />
+      <input name="firstName" />
+      <input name="ico" />
+      <input name="dic" />
+      <input name="lastName" />
+      <input name="phone" />
+      <input name="customerEmail]" />
+      <select name="shippingMethod"></select>
+      <textarea name="orderNotes" />
+      <input name="taxPrice" />
+      <input name="totalPriceWithTaxes" />
+      <input name="items" />
+    </form>
     <div className="custom-container mb-5 pb-5 pt-5" style={{display: "none"}}>
       <div className="row">
         <div className="col-12 col-sm-6 col-md-4">
